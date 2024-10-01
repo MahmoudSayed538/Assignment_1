23b0e4b66fc452a76bd48a06d83b6ce924b3d2c4
Mahmoud Sayed Mahmoud Younis <saitama@kali.com>
2024-10-01 16:31:36 +0300

    almost done

--- A1_T1_SectionNum_20230372.cpp
+++ A1_T1_SectionNum_20230372.cpp
@@ -9,71 +9,101 @@
 //explantion.
 
 #include<bits/stdc++.h>
-#include <cstdlib>
-#include <iostream>
-#include <regex>
-#include <string>
-#include <variant>
 using namespace std;
 
 // menu.
-
-bool badInput(string &Input)
+bool badInput(string &Input,string good)
 {
-    regex goodInput("[ ]*[0,2,5,8,11]?[ ]*");
-    bool good = regex_match(Input,goodInput);
-    if(!good)
+    regex goodInput(good);
+    bool okay = regex_match(Input,goodInput);
+    if(!okay)
     {
         return true;
     }else{
         return false;
     }
-}
-
-string menu()
+}regex val(string s)
 {
-    cout<< "Choose the problem you wish to solve (2,5,8,11)\nor type 0 to exit\n";
-    string choice;
-    getline(cin,choice);
-    while(badInput(choice))
-    {
-        cout<<"Error, you Input is not correct\nPlease try again and make sure to choose from (2,5,8,11) as your input\n";
-        getline(cin,choice);
-        badInput(choice);
-    }
-    return choice;
-
+    regex r (s);
+    return r;
 }
 
-string problem_2()
-{
-    string message;
-    getline(cin,message);
-    string masculine[3]={"he","him","his"};
-    string feminine[3]={"she","her","hers"};
-    // to be continued.
+class menu {
+    public:
+        string input;
+        string good="[ ]*[0,2,5,8,11][ ]*";
+        menu()
+        {
+              cout<< "Choose the problem you wish to solve (2,5,8,11)\nor type 0 to exit\n";
+              getline(cin,input);
+              while(badInput(input,good))
+              {
+                  cout<<"Error, you Input is not correct\nPlease try again and make sure to choose from (2,5,8,11) as your input or type 0 to exit\n";
+                  getline(cin,input);
+                  badInput(input,good);
+              }
+        }
+};
 
-}
+
+class problem_2{
+    public:
+        problem_2()
+        {
+            cout<<"code is okay\n";
+        }
+};
+class problem_5{
+    public:
+        problem_5()
+        {
+            cout<<"code is okay\n";
+        }
+
+};
+class problem_8{
+    public:
+        problem_8()
+        {
+            cout<<"code is okay\n";
+        }
+
+};
+class problem_11{
+    public:
+        problem_11()
+        {
+            cout<<"code is okay\n";
+        }
+
+};
 
 int main()
 {
     while(true)
     {
-        if(menu()=="0")
+        menu *Menu = new menu();
+        if(regex_match(Menu->input,val("[ ]*2[ ]*")))
         {
+            problem_2 *solution=new problem_2();
+            delete(solution);
+
+        }else if(regex_match(Menu->input,val("[ ]*5[ ]*")))
+        {
+            problem_5 *solution=new problem_5();
+            delete(solution);
+        }else if(regex_match(Menu->input,val("[ ]*8[ ]*")))
+        {
+            problem_8 *solution=new problem_8();
+            delete(solution);
+        }else if(regex_match(Menu->input,val("[ ]*11[ ]*")))
+        {
+            problem_11 *solution=new problem_11();
+            delete(solution);
+        }else{
             cout<<"Thank you for using the program\n";
+            delete(Menu);
             break;
-        }else if (menu()=="2")
-        {
-            problem_2();
-        }else if (menu()== "5")
-        {
-            //code for problem 5;
-        }else if (menu()=="8")
-        {
-            //code for problem 8;
-        }else {
-            // code for problem 11;
         }
     }
 }
